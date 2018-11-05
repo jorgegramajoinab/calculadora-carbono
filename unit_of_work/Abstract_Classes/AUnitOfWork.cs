8a38aec4a28887afa1acace003cf9e0f72248db5
Jorge Gramajo <jorge.gramajo@inab.gob.gt>
2018-11-05 10:53:15 -0600

    Added a new vs c# class library project with the design pattern unit of work

---
+++ unit_of_work/Abstract_Classes/AUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_of_work.Abstract_Classes
+{
+    public abstract class AUnitOfWork<Type> : IUnitOfWork
+    {
+        public Type Result { get; set; }
+
+        public abstract void Run();
+    }
+}
