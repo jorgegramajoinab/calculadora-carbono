85371499d5a0f0550e5ddd4f794a828d71d89639
Jorge Gramajo <jorge.gramajo@inab.gob.gt>
2018-11-14 11:18:43 -0600

    Added math.js library and new files.

---
+++ carbon_calculator/Scripts/utilities/utilities.js
@@ -0,0 +1,24 @@
+﻿    function apiGet( url ) {
+        $.ajax("api/species/all")
+            .done(data => console.log(data))
+            .fail(() => console.log("Error."))
+            .always(() => console.log("Completado."));
+        //$.ajax({
+        //    url: url,
+        //    method: "GET",
+        //    data: {
+        //        especie: especie
+        //    },
+        //    success: function (data) {
+        //        var status = data.status;
+        //        // Llamada correcta, muestra resultado
+        //        if (status == 200) {
+        //            $('#sel_sitio').html('');
+        //            $('#sel_sitio').select2({ data: data.response });
+        //        }
+        //    },
+        //    error: function (requestObject, error, errorThrown) {
+        //        alert('Error al obtener índices de sitio');
+        //    }
+        //});
+    }
